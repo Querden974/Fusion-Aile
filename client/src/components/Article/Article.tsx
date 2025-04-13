f8a766fbec1b938fb73ee17ee87b608b2b09da70
QuerdenVFX <gautier.ray.vfx@gmail.com>
2025-04-13 14:51:30 +0200

    Edit Popup Dialog

--- client/src/components/Article/Article.tsx
+++ client/src/components/Article/Article.tsx
@@ -11,28 +11,41 @@ interface ArticleTypes{
 
 export default function Article({ article }:{article:ArticleTypes}):ReactElement {
 
+    const isObject:boolean = typeof article.bienfaits[0] === 'object'
+    const isString:boolean = typeof article.bienfaits[0] === 'string'
+
 
     return (
-        <div className="h-fit">
+        <div className="h-fit w-full">
+
             <h2 className="font-bold">Bienfaits :</h2>
-            <div className="flex flex-col gap-2 items-start">
-                {article.bienfaits.map((item:string|SubArticle, index:number):ReactElement => {
-                    if (typeof item === 'string') {
-                        return <li key={index} className="list-disc ml-5">{item}</li>;
-                    } else if (typeof item === 'object' && 'title' in item && 'biens' in item) {
-                        return (
-                            <div key={index} className="flex flex-col gap-1 items-start">
-                                <h3 className="font-semibold">{item.titre}</h3>
-                                <ul>
-                                    {item.biens.map((bien:string, i:number):ReactElement => (
-                                        <li key={i} className="list-disc ml-10">{bien}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        );
-                    }
-                    return null;
-                })}
+            <div className={` gap-8 items-start ${isObject ? 'grid grid-cols-2' : 'flex flex-col'}`}>
+                {isString &&
+                    <ul>
+                        {article.bienfaits.map((item:string, index:number):ReactElement => (<li key={index} className="list-disc ml-5">{item}</li>))}
+                    </ul>
+                }
+                {isObject && article.bienfaits.map((item:SubArticle, index:number):ReactElement => {
+                            if (typeof item === 'object') {
+                                if( "titre" in item && "biens" in item) {
+                                    return (
+                                        <div key={index} className="flex flex-col gap-1 items-start">
+
+                                            <h3 className="font-semibold">{item.titre}</h3>
+                                            <ul className={"flex flex-col gap-1 items-start list-inside"}>
+                                                {item.biens.map((bien:string, i:number):ReactElement => (
+                                                    <li key={i} className="list-disc">{bien}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    );
+                                }
+
+                            }
+                            return null;
+                        })
+                }
+
             </div>
         </div>
     )
--- client/src/components/Popup/Popup.tsx
+++ client/src/components/Popup/Popup.tsx
@@ -23,6 +23,7 @@ type PopupTypes = {
 }
 
 export default function Popup({prestation, index, x, y}:PopupTypes):ReactElement {
+    const isObject:boolean = typeof prestation.bienfaits[0] === "object";
 
     return (
         <Dialog>
@@ -40,9 +41,9 @@ export default function Popup({prestation, index, x, y}:PopupTypes):ReactElement
                 </button>
 
             </DialogTrigger>
-            <DialogContent className={"bg-neutral"}>
+            <DialogContent className={"bg-neutral min-w-2xl"}>
                 <DialogHeader>
-                    <DialogTitle className={"w-fit"} style={Array.isArray(prestation.couleur)
+                    <DialogTitle className={"w-fit text-xl"} style={Array.isArray(prestation.couleur)
                         ? {
                             backgroundImage: `linear-gradient(to right, ${prestation.couleur[0]}, ${prestation.couleur[1]})`,
                             WebkitBackgroundClip: "text",
@@ -51,13 +52,19 @@ export default function Popup({prestation, index, x, y}:PopupTypes):ReactElement
                         : {color: prestation.couleur}
                     }>{prestation.nom}</DialogTitle>
                 </DialogHeader>
-                <DialogDescription className={"text-justify"}>
+                <DialogDescription className={"text-justify "}>
                     <div className="flex flex-col gap-4 h-fit ">
                         <div className="grid grid-cols-2 gap-3 w-full">
-                            <p className={"text-start"}>{prestation.description}</p>
+                            <div className="flex flex-col gap-6">
+                                <p className={"container whitespace-break-spaces h-fit text-start text-[clamp(1.5cqh,1.75cqh,4rem)]"}
+                                    >{prestation.description}</p>
+
+                                {(!isObject && prestation.bienfaits.length <=4 && prestation.bienfaits.length >1 ) && <Article article={prestation} />}
+                            </div>
+
                             <img src={`/massages/${prestation.code}.webp`} className={" rounded-box"} alt=""/>
                         </div>
-                        <Article article={prestation} />
+                        {(isObject || prestation.bienfaits.length > 4 || prestation.bienfaits.length === 1) && <Article article={prestation} />}
                         <p className={"text-center min-w-1/3 mx-auto  p-4 rounded-box font-semibold"}
                            style={Array.isArray(prestation.couleur)
                                ? {
